72867a56afd611f3ffe1b1eebdf6c1537279a564
Leonie <leonie.alexandra22@gmail.com>
2023-01-23 17:23:29 -0500

    Fix bug in register

--- html/LAMPAPI/Register.php
+++ html/LAMPAPI/Register.php
@@ -4,10 +4,10 @@ include 'Util.php';
 $inData = getRequestInfo();
 
 //get user info
-$firstName = $inData["FirstName"];
-$lastName = $inData["lastName"];
-$login = $inData["login"];
-$password = $inData["password"];
+$FirstName = $inData["firstName"];
+$LastName = $inData["lastName"];
+$Login = $inData["login"];
+$Password = $inData["password"];
 
 $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 if( $conn->connect_error )
@@ -18,14 +18,14 @@ else
 {
   //prepare and bind
   $stmt = $conn->prepare("SELECT * FROM Users WHERE Login = ?");
-  $stmt->bind_param("s", $login);
+  $stmt->bind_param("s", $Login);
   $stmt->execute();
   $result = $stmt->get_result();
 
   //if no one else has the same login or username, create new
   if(mysqli_num_rows($result) == 0){
     $stmt = $conn->prepare("INSERT into USERS (FirstName, LastName, Login, Password) Values (?,?,?,?)");
-    $stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+    $stmt->bind_param("ssss", $FirstName, $LastName, $Login, $Password);
     $stmt->execute();
 
     //returns value generated for an auto_increment column or id column
