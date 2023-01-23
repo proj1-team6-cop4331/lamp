d178d0f7db38ffc00504efeae932235d1bceb8cd
John Sierra <sierrajp73@gmail.com>
2023-01-23 18:45:25 -0500

    Last Try

--- html/LAMPAPI/Login.php
+++ html/LAMPAPI/Login.php
@@ -11,20 +11,13 @@ $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 if ($conn->connect_error) {
   returnWithError($conn->connect_error);
 } else {
-  $stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Users WHERE Login=? AND Password =?");
-if( $conn->connect_error )
-{
-  returnWithError( $conn->connect_error );
-}
-else
-{
-  $stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Users WHERE Login=? AND Password =?");
+  $stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
   $stmt->bind_param("ss", $inData["login"], $inData["password"]);
   $stmt->execute();
   $result = $stmt->get_result();
 
   if ($row = $result->fetch_assoc()) {
-    returnWithInfo($row['FirstName'], $row['LastName'], $row['ID']);
+    returnWithInfo($row['firstName'], $row['lastName'], $row['ID']);
   } else {
     returnWithError("No Records Found");
   }
