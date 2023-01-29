95fc28fa52fa6c6d4c1d81764da8e6655c5d7c31
Blake Gassman <51721851+gblake31@users.noreply.github.com>
2023-01-29 12:53:09 -0500

    Set up Contacts Grid (Front End)

--- html/js/profile.js
+++ html/js/profile.js
@@ -13,3 +13,21 @@ function doLogout() {
 function createContact() {
     console.log("Add Contact was pressed, we need to add the form");
 }
+
+function addPlaceholder() {
+    let grid = document.getElementById("grid");
+
+    let contact = {
+        firstName: "Test",
+        lastName: "User",
+        phoneNumber: "123-456-7890",
+        email: "myemail.contactme.com",
+        options: ""
+    }
+
+    for (const property in contact) {
+        let newDiv = document.createElement("div");
+        newDiv.innerHTML = contact[property];
+        grid.appendChild(newDiv);
+    }
+}
