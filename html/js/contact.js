997c2f8e7cfa80a6f112bc141d5f26a439bed028
Blake Gassman <51721851+gblake31@users.noreply.github.com>
2023-02-02 21:54:02 -0500

    Connect Front-end with Search API (wish me luck)

--- html/js/contact.js
+++ html/js/contact.js
@@ -314,3 +314,58 @@ function trash() {
     }
  
 }
+
+function doSearch() {
+    let searchString = document.getElementById("searchBar").value;
+    console.log("Let's search for " + searchString);
+
+    // Create a javascript object containing the stuff we want to send to the API
+    var packageItUp = {
+        search: searchString,
+        userId: window.localStorage.getItem("id")
+    };
+
+    let jsonPayload = JSON.stringify(packageItUp);
+
+    let url = "https://lamp-cop4331.skyclo.dev/LAMPAPI/SearchContact.php";
+    
+    // Create a request
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+
+    // What the heck does this do?
+    xhr.setRequestHeader("Content-type", "application/json; charset = utf-8");
+
+    try {
+        // anonymous function to the gets called when the request is ready
+        xhr.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+
+                // When we get a response from API...
+                var jsonObject = JSON.parse(xhr.responseText);
+                let arr = jsonObject.results;
+
+                // Delete everything else.
+
+                let grid = document.getElementById("grid");
+                for (let i = 10; i < grid.children.length; i++) {
+                    grid.removeChild(grid.children[i]);
+                }
+
+                // Add back the stuff that the search query found.
+
+                for (let i = 0; i < arr.length; i++) {
+                    appendContactList(arr[i]);
+                }
+            }
+        }
+
+        // can we put this above the "ready state change?"
+        // That would make a lot more sense.
+        xhr.send(jsonPayload);
+    }
+
+    catch(theError) {
+        console.error(theError.message);
+    }
+} 
