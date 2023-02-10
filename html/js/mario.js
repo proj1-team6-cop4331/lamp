b710010de9cfcd35eef06ea72ebbd815546ab264
Grafton LeGare <grafton.legare355@knights.ucf.edu>
2023-02-09 20:47:39 -0500

    Separated javascript for mario.html into mario.js

---
+++ html/js/mario.js
@@ -0,0 +1,28 @@
+//Obtain references to login and register DIVs
+
+let selectLogin = document.getElementById("selectLogin");
+console.log(selectLogin.children);
+let selectRegister = document.getElementById("selectRegister");
+
+//Add event listeners for the hover effects, and clicking the DIVs
+selectLogin.addEventListener("mouseover", function () {
+    selectLogin.children[0].classList.remove("hidden");
+});
+selectLogin.addEventListener("mouseout", function () {
+    selectLogin.children[0].classList.add("hidden");
+});
+selectRegister.addEventListener("mouseover", function () {
+    selectRegister.children[0].classList.remove("hidden");
+});
+selectRegister.addEventListener("mouseout", function () {
+    selectRegister.children[0].classList.add("hidden");
+});
+
+selectLogin.addEventListener("onclick", function () {
+    console.log("Login clicked.");
+    window.location.href = "login.html";
+});
+selectRegister.addEventListener("onclick", function () {
+    console.log("Register clicked.");
+    window.location.href = "register.html";
+});
